0408ef4e8f8e3e6b64fad70b141c2a1800a371a4
Ken Moini <ken@kenmoini.com>
2019-06-23 16:56:59 -0500

    Testing the fit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -64,6 +64,20 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
         </style>
 
     </head>
