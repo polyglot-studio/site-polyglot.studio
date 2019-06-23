3863cd16a732b46a0d985191963a2a384a1502d5
Ken Moini <ken@kenmoini.com>
2019-06-23 18:39:09 -0500

    navjsfml

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,14 +1,18 @@
 function navigationLinkClick(e) {
+  console.log('func fired');
   if (jQuery("nav#mainav").hasClass('nav-is-bot')) {
+    console.log('is bot');
     jQuery("nav#mainav").removeClass('nav-is-bot').addClass("nav-is-top").attr('style','bottom:inherit;');
   }
   else {
+    console.log('is top');
     jQuery("nav#mainav").removeClass('nav-is-top').addClass("nav-is-bot").attr('style','bottom:0;');
   }
 }
 
 jQuery(document).ready(function() {
   jQuery("nav#mainnav").on('click', 'a', function(e) {
+    console.log('click fired');
     e.preventDefault();
     navigationLinkClick(e);
   })
