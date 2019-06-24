b2c319e1d7f151c132975c394accf4d3279c833c
Ken Moini <ken@kenmoini.com>
2019-06-23 21:05:29 -0500

    navjscalcer

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,6 +1,6 @@
 function navigationLinkClick(e) {
   console.log('func fired');
-  var navheight = jQuery("nav#mainnav").height();
+  var navheight = jQuery("nav#mainnav").outerHeight();
   if (jQuery("nav#mainnav").hasClass('nav-is-bot')) {
     console.log('is bot');
     jQuery("nav#mainnav").removeClass('nav-is-bot').addClass("nav-is-top").attr('style','bottom:calc(100vh - ' + navheight + 'px);');
