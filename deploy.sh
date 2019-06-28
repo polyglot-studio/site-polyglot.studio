56c60e242d880a1f160bb0596cd2540d945c097d
Ken Moini <ken@kenmoini.com>
2019-06-28 15:56:48 +0000

    who knows if this'll work

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,19 @@
 # activate maintenance mode
 php artisan down
 
+chmod -R 775 .
+
 # update source code
 git fetch --all
 git reset --hard origin/master
 
+chmod -R 775 .
+
 # update views and frontend dependancies
 npm install
 npm run dev
+
+chmod -R 775 .
 
 # update PHP dependencies
 composer install --no-interaction --prefer-dist
@@ -17,9 +23,13 @@ composer install --no-interaction --prefer-dist
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 
+chmod -R 775 .
+
 # update database
 php artisan migrate --force
 # --force  Required to run when in production.
+
+chmod -R 775 .
 
 # stop maintenance mode
 php artisan up
--- webhook.js
+++ webhook.js
@@ -3,16 +3,35 @@ require('dotenv').config()
 const secret = process.env.APP_DEPLOY_SECRET;
 const repo = "/var/www/polyglot.studio/";
 
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
 
-http.createServer(function (req, res) {
+//http.createServer(function (req, res) {
+//    req.on('/webhooks/github/deploy', function(chunk) {
+//        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+//        if (req.headers['x-hub-signature'] == sig) {
+//            exec('cd ' + repo + ' && chmod -R 775 . && ./deploy.sh && chmod -R 775 .');
+//        }
+//    });
+
+//    res.end();
+//}).listen(6969);
+
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/polyglot.studio/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/polyglot.studio/cert.pem')
+};
+
+https.createServer(options, function (req, res) {
     req.on('/webhooks/github/deploy', function(chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
         if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && chmod -R 775 . && ./deploy.sh && chmod -R 775 .');
+            exec('cd ' + repo + ' && && ./deploy.sh');
         }
     });
 
